115f7101ea38093c14de242391cd6b8f5e7de621
dograkunal <kunaldogra31@gmail.com>
2022-02-28 20:37:49 +0530

    merging

---
+++ apps/frontend/src/app/views/dashboard/pages/todo/todoList.js
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getList, getRemove } from './todoRedux/todoActions';
+import { useNavigate } from 'react-router-dom';
+
+function TodoList() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(getList());
+  }, []);
+
+  const list = useSelector((state) => state.todo.tasksList);
+  // const help = useSelector((state) => state);
+  // eslint-disable-next-line no-debugger
+  // debugger;
+  console.log(list, 'List data');
+
+  return (
+    <>
+      <div>
+        <h1>To Do List</h1>
+      </div>
+      <div>
+        <button onClick={() => navigate('/dashboard/add', { replace: true })}>
+          Add a new Data
+        </button>
+      </div>
+      <table border="1">
+        <tbody>
+          <tr>
+            <td>Task Creator</td>
+            <td>To Do Task</td>
+            <td>Is Immediate</td>
+            <td>Delete</td>
+            <td>Edit</td>
+          </tr>
+          {list &&
+            list.map((data, index) => (
+              <tr key={index}>
+                <td>{data && data.creator}</td>
+                <td>{data && data.task}</td>
+                <td>{data && data.isImmediate}</td>
+                <td>
+                  <button onClick={() => dispatch(getRemove(data.taskId))}>
+                    delete
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() =>
+                      navigate(`/dashboard/edit/${data.taskId}`, {
+                        replace: true,
+                      })
+                    }
+                  >
+                    Edit data
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default TodoList;
